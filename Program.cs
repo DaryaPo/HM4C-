c00a9e656efc7e59013d50e6d3a22b7a4acb4f11
Darya <datya@mail.ru>
2022-09-04 21:04:36 +0300

    ggjc

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿void FillArray (int[]array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array [i] = new Random().Next(1,100);
+        
+}
+}
+int [] array = new int [8];
+FillArray(array);
+Console.WriteLine("[{0}]", string.Join(",", array));
